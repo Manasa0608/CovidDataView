5fba822354f982be2213583ddfffc15e29464f20
Manasa0608 <60744666+Manasa0608@users.noreply.github.com>
2020-04-03 12:16:15 +0530

    Add files via upload

---
+++ CovidUpdate/src/test/java/com/manasa/CovidUpdate/CovidUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manasa.CovidUpdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
